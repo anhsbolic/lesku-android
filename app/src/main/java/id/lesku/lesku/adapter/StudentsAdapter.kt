cc40ebd9cab4c2f9f90de9034e2beb06a25c330e
anhsbolic <anhsbolic@gmail.com>
2018-02-09 10:12:51 +0700

    [Students] update layout: add drawable initial text

--- app/src/main/java/id/lesku/lesku/adapter/StudentsAdapter.kt
+++ app/src/main/java/id/lesku/lesku/adapter/StudentsAdapter.kt
@@ -5,7 +5,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.amulyakhare.textdrawable.TextDrawable
+import com.amulyakhare.textdrawable.util.ColorGenerator
 import id.lesku.lesku.R
 import id.lesku.lesku.model.Student
 import kotlinx.android.synthetic.main.fragment_students_adapter_list.view.*
@@ -21,6 +24,7 @@ class StudentsAdapter(private val dataStudents: ArrayList<Student>,
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        internal var imgStudent: ImageView = itemView.studentsAdapterListImg
         internal var txtStudentName : TextView = itemView.studentsAdapterListName
         internal var txtStudentGradeLevel : TextView = itemView.studentsAdapterListGradeLevel
         internal var txtStudentSchool : TextView = itemView.studentsAdapterListSchool
@@ -39,7 +43,19 @@ class StudentsAdapter(private val dataStudents: ArrayList<Student>,
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.txtStudentName.text = dataStudents[position].name!!
+        val studentName = dataStudents[position].name!!
+        val initialName = studentName[0].toString()
+        val colorGenerator: ColorGenerator = ColorGenerator.MATERIAL
+        val color: Int = colorGenerator.getColor(initialName)
+        val textDrawable: TextDrawable = TextDrawable.builder().beginConfig()
+                .width(100)
+                .height(100)
+                .endConfig()
+                .buildRound(initialName, color)
+
+        holder.imgStudent.setImageDrawable(textDrawable)
+
+        holder.txtStudentName.text = studentName
         holder.txtStudentGradeLevel.text = dataStudents[position].grade_level!!
         holder.txtStudentSchool.text = dataStudents[position].school!!
         holder.txtStudentAddress.text = dataStudents[position].address!!
