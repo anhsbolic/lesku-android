475c33303e4762bc6123b84e1e61d91337f7bfdb
anhsbolic <anhsbolic@gmail.com>
2018-02-14 14:45:23 +0700

    Set navigation between ScheduleFragment and CreateScheduleActivity. updatemanifest setting

--- app/src/main/java/id/lesku/lesku/activity/CreateScheduleActivity.kt
+++ app/src/main/java/id/lesku/lesku/activity/CreateScheduleActivity.kt
@@ -1,7 +1,10 @@
 package id.lesku.lesku.activity
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.inputmethod.InputMethodManager
 import id.lesku.lesku.R
 
 class CreateScheduleActivity : AppCompatActivity() {
@@ -9,5 +12,27 @@ class CreateScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_schedule)
+        setTitle(R.string.create_schedule_activity_title)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                //showKeyboard(false)
+                onBackPressed()
+                true
+            }
+            else -> {
+                super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    private fun showKeyboard(showKeyboard: Boolean){
+        (getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager)
+                .hideSoftInputFromWindow(currentFocus!!.windowToken,
+                        if (showKeyboard) InputMethodManager.SHOW_FORCED
+                        else InputMethodManager.HIDE_NOT_ALWAYS )
     }
 }
