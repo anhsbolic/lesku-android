585a5e1bd3db08d330b2974d20e61cb954f9c22b
anhsbolic <anhsbolic@gmail.com>
2018-02-18 15:06:36 +0700

    create SingleSchedule data model

---
+++ app/src/main/java/id/lesku/lesku/model/SingleSchedule.kt
@@ -0,0 +1,27 @@
+package id.lesku.lesku.model
+
+import android.annotation.SuppressLint
+import android.os.Parcelable
+import android.support.annotation.Keep
+import kotlinx.android.parcel.Parcelize
+import java.util.*
+
+@Keep
+@SuppressLint("ParcelCreator")
+@Parcelize
+data class SingleSchedule(var id_single_schedule: Int?,
+                          var created_date: Date?,
+                          var updated_date: Date?,
+                          var id_schedule: Int?,
+                          var date: Date?,
+                          var subject: String?,
+                          var notes: String?) : Parcelable {
+    constructor(): this(
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null)
+}
